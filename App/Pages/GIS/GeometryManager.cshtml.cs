18170d7645d3fa38816241ae917a829ad3094857
surfsky <surfsky@sina.com>
2026-06-10 23:11:46 +0800

    geometrymap

--- App/Pages/GIS/GeometryManager.cshtml.cs
+++ App/Pages/GIS/GeometryManager.cshtml.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using App.Components;
 using App.DAL;
 using App.DAL.GIS;
-using Microsoft.AspNetCore.Authorization;
 
 namespace App.Pages.GIS
 {
@@ -12,7 +11,10 @@ namespace App.Pages.GIS
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public long? MenuId { get; set; }
         public string Url { get; set; }
+        public string ListUrl { get; set; }
+        public string MapUrl { get; set; }
         public List<GeometryNavNode> Children { get; set; } = new();
     }
 
@@ -20,13 +22,15 @@ namespace App.Pages.GIS
     public class GeometryManagerModel : AdminModel
     {
         public List<GeometryNavNode> NavMenus { get; set; } = new();
-        public string DefaultGeometriesUrl { get; set; } = "/GIS/Geometries";
+        public string CurrentMode { get; set; } = "list";
+        public string DefaultFrameUrl { get; set; } = "/GIS/Geometries";
 
-        public void OnGet(long? menuId)
+        /// <summary>加载页面</summary>
+        public void OnGet(long? menuId, string mode)
         {
+            CurrentMode = mode == "map" ? "map" : "list";
             var menus = GisMenu.GetTree().OrderBy(x => x.SortId).ThenBy(x => x.Id).ToList();
-            if (menuId.HasValue)
-                DefaultGeometriesUrl = $"/GIS/Geometries?menuId={menuId.Value}";
+            DefaultFrameUrl = BuildUrl(menuId, CurrentMode == "map");
 
             NavMenus = new List<GeometryNavNode>
             {
@@ -34,12 +38,15 @@ namespace App.Pages.GIS
                 {
                     Id = "all",
                     Name = "全部图层",
-                    Url = "/GIS/Geometries",
+                    Url = BuildUrl(null, CurrentMode == "map"),
+                    ListUrl = BuildUrl(null, false),
+                    MapUrl = BuildUrl(null, true),
                     Children = BuildNavNodes(menus)
                 }
             };
         }
 
+        /// <summary>构建导航节点</summary>
         private static List<GeometryNavNode> BuildNavNodes(IEnumerable<GisMenu> menus)
         {
             if (menus == null)
@@ -52,10 +59,20 @@ namespace App.Pages.GIS
                 {
                     Id = x.Id.ToString(),
                     Name = x.Name,
-                    Url = $"/GIS/Geometries?menuId={x.Id}",
+                    MenuId = x.Id,
+                    Url = BuildUrl(x.Id, false),
+                    ListUrl = BuildUrl(x.Id, false),
+                    MapUrl = BuildUrl(x.Id, true),
                     Children = BuildNavNodes(x.Children)
                 })
                 .ToList();
+        }
+
+        /// <summary>构建目标地址</summary>
+        private static string BuildUrl(long? menuId, bool isMap)
+        {
+            var baseUrl = isMap ? "/GIS/GeometryMap" : "/GIS/Geometries";
+            return menuId.HasValue ? $"{baseUrl}?menuId={menuId.Value}" : baseUrl;
         }
     }
 }
--- App/Pages/GIS/GeometryMap.cshtml.cs
+++ App/Pages/GIS/GeometryMap.cshtml.cs
@@ -12,32 +12,34 @@ namespace App.Pages.GIS
     {
         public void OnGet() { }
 
+        /// <summary>获取地图图层点位数据</summary>
         public JsonResult OnGetGeometryLayerData(long? menuId)
         {
             var query = GisGeometry.Search(menuId: menuId, isValid: true, recursive: true);
             var list = query
                 .OrderBy(g => g.SortId)
                 .ThenBy(g => g.Id)
-                .Select(g => new
-                {
-                    id = g.Id,
-                    type = g.Type,
-                    menuId = g.MenuId,
-                    name = g.Name,
-                    alias = g.Alias,
-                    sortId = g.SortId,
-                    addr = g.Addr,
-                    gps = g.Gps,
-                    region = g.Region,
-                    url = g.Url,
-                    file = g.File,
-                    geoJson = g.GeoJson,
-                    dataJson = g.DataJson,
-                    icon = g.Menu != null ? g.Menu.Icon : null
-                })
+                .Select(SelectItem())
                 .ToList();
 
             return BuildResult(0, "success", list);
+        }
+
+        /// <summary>获取单个点位数据</summary>
+        public JsonResult OnGetGeometryItem(long id)
+        {
+            if (id <= 0)
+                return BuildResult(400, "参数错误");
+
+            var item = GisGeometry.Search(isValid: true)
+                .Where(g => g.Id == id)
+                .Select(SelectItem())
+                .FirstOrDefault();
+
+            if (item == null)
+                return BuildResult(404, "点位不存在");
+
+            return BuildResult(0, "success", item);
         }
 
         public IActionResult OnPostDeleteGeometry([FromBody] DeleteReq req)
@@ -72,6 +74,28 @@ namespace App.Pages.GIS
         public class DeleteReq
         {
             public long Id { get; set; }
+        }
+
+        /// <summary>点位投影</summary>
+        private static System.Linq.Expressions.Expression<System.Func<GisGeometry, object>> SelectItem()
+        {
+            return g => new
+            {
+                id = g.Id,
+                type = g.Type,
+                menuId = g.MenuId,
+                name = g.Name,
+                alias = g.Alias,
+                sortId = g.SortId,
+                addr = g.Addr,
+                gps = g.Gps,
+                region = g.Region,
+                url = g.Url,
+                file = g.File,
+                geoJson = g.GeoJson,
+                dataJson = g.DataJson,
+                icon = g.Menu != null ? g.Menu.Icon : null
+            };
         }
     }
 }
